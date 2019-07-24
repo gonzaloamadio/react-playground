44ff1d8e9b0d21843cd51a745510d575ed2e7454
gonzaloamadio <gonzaloamadio@gmail.com>
2019-07-24 16:02:42 +1000

    Add some styling to components

--- src/components/events-functions/pass-methods-btw-components.js
+++ src/components/events-functions/pass-methods-btw-components.js
@@ -33,14 +33,29 @@ class Pmbc extends Component {
     } )
   }
 
+
   render () {
+    // Super Basic ways of styling, with this type not all the power of css can be used.
+    const style = {
+        backgroundColor: 'white',
+        font: 'inherit',
+        border: '1px solid blue',
+        padding: '8px',
+        cursor: 'pointer'
+    }
+
     return (
+        
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         {/* Pass a parameter and bind this to this class context. ARROW FUNCTION WAY */}
         {/* This can be inefficient, and bind way is preferred */}
-        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <button 
+            style={style} //Add inline style
+            onClick={() => this.switchNameHandler('Maximilian!!')}>
+            Switch Name
+        </button>
         <Person 
           name={this.state.persons[0].name} 
           age={this.state.persons[0].age} />
