052c0a975a2ebb2591a2f7f0cda441478ac975f3
gonzaloamadio <gonzaloamadio@gmail.com>
2019-07-24 15:20:12 +1000

    Pass function to child on props. Bind fc to actual class context

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import StateClassBased from './components/state/class-based'
-import StateHook from './components/state/use-state-hoook'
+// import StateClassBased from './components/state/class-based'
+// import StateHook from './components/state/use-state-hoook'
+import Pmbc from './components/events-functions/pass-methods-btw-components'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StateClassBased />
-        <StateHook />
+        {/* <StateClassBased /> */}
+        {/* <StateHook /> */}
+        <Pmbc />
       </div>
     );
   }
---
+++ src/components/events-functions/pass-methods-btw-components.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Person from './Person';
+
+class Pmbc extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
+
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        {/* Pass a parameter and bind this to this class context. ARROW FUNCTION WAY */}
+        <button onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+//        Pass a parameter and bind this to this class context. EXPLICIT BIND WAY
+          click={this.switchNameHandler.bind(this, 'Max!')}
+//        Pass a function to child. It is an arrow function, so it binds "this" to this class.
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default Pmbc;
